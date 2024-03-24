54265b5e1889f33d20603778636bc3bb7a49392e
02shreyansh <02.shreyansh.10@gmail.com>
2024-03-24 23:04:21 +0530

    changes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.main'),
-//     smooth: true
-// });
+
+
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
 
 
 
